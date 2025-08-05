8fa6977909f52f294673bd3f14ddcffc4a574971
vlanuka <andreychuk04@mail.ru>
2025-08-05 10:14:12 +0300

    feat: create linear regression model

---
+++ linear_regression.py
@@ -0,0 +1,69 @@
+from __future__ import annotations
+
+from typing import List
+
+import numpy as np
+
+from descents import BaseDescent
+from descents import get_descent
+
+
+class LinearRegression:
+    """
+    Linear regression class
+    """
+
+    def __init__(self, descent_config: dict, tolerance: float = 1e-4, max_iter: int = 300):
+        """
+        :param descent_config: gradient descent config
+        :param tolerance: stopping criterion for square of euclidean norm of weight difference (float)
+        :param max_iter: stopping criterion for iterations (int)
+        """
+        self.descent: BaseDescent = get_descent(descent_config)
+
+        self.tolerance: float = tolerance
+        self.max_iter: int = max_iter
+
+        self.loss_history: List[float] = []
+        self.iteration = 0
+
+    def fit(self, x: np.ndarray, y: np.ndarray) -> LinearRegression:
+        """
+        Fitting descent weights for x and y dataset
+        :param x: features array
+        :param y: targets array
+        :return: self
+        """
+        # TODO: fit weights to x and y
+        
+        while True:
+            self.iteration+=1
+            
+            gradient = self.descent.calc_gradient(x, y)
+            weights_diff = self.descent.update_weights(gradient)
+            weights_diff_L2 = np.linalg.norm(weights_diff)
+            nan_mask = np.isnan(weights_diff)
+            self.loss_history.append(self.descent.calc_loss(x, y))
+            
+            if weights_diff_L2<self.tolerance or np.any(nan_mask) or self.iteration>=self.max_iter:
+                break
+            
+
+        return self
+        # raise NotImplementedError('LinearRegression fit function not implemented')
+
+    def predict(self, x: np.ndarray) -> np.ndarray:
+        """
+        Predicting targets for x dataset
+        :param x: features array
+        :return: prediction: np.ndarray
+        """
+        return self.descent.predict(x)
+
+    def calc_loss(self, x: np.ndarray, y: np.ndarray) -> float:
+        """
+        Calculating loss for x and y dataset
+        :param x: features array
+        :param y: targets array
+        """
+        return self.descent.calc_loss(x, y)
